71fc896c8d5b110d7623053379d0359b716ae05d
Francisco Giordano <frangio.1@gmail.com>
2016-08-20 01:07:36 -0300

    arregla bug (ver test 4)

--- pap-tp1/ej3/ej3.cpp
+++ pap-tp1/ej3/ej3.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int n, a, b;
-  cin >> n >> a; // n es positivo
-  int m = a;
-  for(int i = 1; i < n; ++i) {
+  int n, b, a = 0, m = 0;
+  cin >> n; // n es positivo
+  for(int i = 0; i < n; ++i) {
     cin >> b;
     a = max(0, a + b);
     m = max(m, a);
