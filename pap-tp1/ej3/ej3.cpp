c0016f1bbba102ed63bcd229620feddc6b27474f
Francisco Giordano <frangio.1@gmail.com>
2016-09-03 20:37:58 -0300

    agrega espacios

--- pap-tp1/ej3/ej3.cpp
+++ pap-tp1/ej3/ej3.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+
 using namespace std;
+
 int main() {
   int n, b, a = 0, m = 0;
+
   cin >> n; // n es positivo
+
   for(int i = 0; i < n; ++i) {
     cin >> b;
     a = max(0, a + b);
     m = max(m, a);
   }
+
   cout << m << endl;
+
   return 0;
 }
