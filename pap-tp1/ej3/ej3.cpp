39d0f103cc2a8c0267486f7c9123c2f9c4383e80
nico <nicoroulet@gmail.com>
2016-09-09 02:29:22 -0300

    int64_t en ej3

--- pap-tp1/ej3/ej3.cpp
+++ pap-tp1/ej3/ej3.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 int main() {
-  int n, b, a = 0, m = 0;
+  int n;
+  int64_t b, a = 0, m = 0;
 
   cin >> n; // n es positivo
 
   for(int i = 0; i < n; ++i) {
     cin >> b;
-    a = max(0, a + b);
+    a = max((int64_t)0, a + b);
     m = max(m, a);
   }
 
