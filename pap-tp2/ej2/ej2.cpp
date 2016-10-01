c64468ccee5b33405886d0f4fba05dfb87e383b8
gus <gushurovich@gmail.com>
2016-10-01 20:50:00 -0300

    ej2

--- pap-tp2/ej2/ej2.cpp
+++ pap-tp2/ej2/ej2.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 #define DBG(x) cerr << #x << " = " << (x) << endl
 
 using namespace std;
 
-int compAcc(vector<int> v1, vector<int> v2){
-	if (v1[0] < v2[0]) {
-	    /* code */
+bool menor(vector<int> &v1, vector<int> &v2){
+	// llamame con dos vectores del mismo tamaño man
+	assert(v1.size() == v2.size());
+
+	for (int i = 0; i < v1.size(); ++i) {
+		if (v1[i] >= v2[i]) {
+			return false;
+		}
+	return true;
 	}
+}
+
+int comp(vector<int> &v1, vector<int> &v2){
+	// 0 si es menor, 1 si es mayor, -1 si no son comparables (se cruzan)
+	if (menor(v1,v2)) return 0;
+	if (menor(v2,v1)) return 1;
+	return -1;
 }
 
 
