9435c7f784d45ea8ee0ff3f418305d72b2b4c0da
Francisco Giordano <frangio.1@gmail.com>
2016-11-01 23:25:16 -0300

    mayusculas

--- pap-tp3/ej3/ej3.py
+++ pap-tp3/ej3/ej3.py
@@ -1,18 +1,18 @@
-c, a, q = (int(w) for w in input().split())
+C, A, Q = (int(w) for w in input().split())
 
 M = []
-for _ in range(c):
+for _ in range(C):
     M.append(list(int(w) for w in input().split()))
 
-S = [[0] * (a+1) for _ in range(c+1)]
+S = [[0] * (A+1) for _ in range(C+1)]
 
-for i in range(c):
-    for j in range(a):
+for i in range(C):
+    for j in range(A):
         S[i+1][j+1] = M[i][j] + S[i][j+1] + S[i+1][j] - S[i][j] 
 
 def query(c1, a1, c2, a2):
     return S[c2][a2] - S[c1][a2] - S[c2][a1] + S[c1][a1]
 
-for _ in range(q):
+for _ in range(Q):
     q0 = (int(w) for w in input().split())
     print(query(*q0))
