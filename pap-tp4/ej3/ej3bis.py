d121f2f1a3d65763420276d09285c0e776ac02f8
nico <nicoroulet@gmail.com>
2016-12-02 00:58:59 -0300

    ej3 parece andar

--- pap-tp4/ej3/ej3bis.py
+++ pap-tp4/ej3/ej3bis.py
@@ -1,4 +1,5 @@
 from math import atan2, pi # atan2(y,x): angulo en radianes del vector (x,y) con respecto a la horizontal, en sentido antihorario
+from sys import exit
 
 class point(tuple):
     def __add__(self, other):
@@ -20,11 +21,11 @@ class triangle(tuple):
         # print(tuple(points))
         a, b, c = tuple(sorted(points))
         # print("etuple ", (a,b,c), (b-a).angle(c-a), atan2(b-a), atan2(c-a))
-        print("building ", (a,b,c) if ((b-a).angle(c-a) < pi) else (a,c,b))
+        # print("building ", (a,b,c) if ((b-a).angle(c-a) < pi) else (a,c,b))
         return super(triangle, self).__new__(self, (a,b,c) if ((b-a).angle(c-a) < pi) else (a,c,b))
     def __contains__(self, point):
         if point in tuple(self): return False
-        print(self)
+        # print(self)
         a, b, c = self
         ab = b - a
         ac = c - a
@@ -43,24 +44,42 @@ triangulos = {} # diccionario punto inferior -> [(triangulo, cant de puntos inte
 for i in range(h):
     for j in range(i+1, h):
         for k in range(j+1, h):
-            print(i,j,k)
+            # print(i,j,k)
             # print(triangle([puntosBuenos[x] for x in [i,j,k]]))
             # t = (puntosBuenos[i],) + tuple(sorted((puntosBuenos[j], puntosBuenos[k]), key = lambda v: (v.angulo((0,1)), abs(v))))
             t = triangle([puntosBuenos[x] for x in [i,j,k]])
-            print("t:",t, "ijk:", [puntosBuenos[x] for x in [i,j,k]])
+            # print("t:",t, "ijk:", [puntosBuenos[x] for x in [i,j,k]])
             if all(not x in t for x in puntosMalos):
                 cantBuenos = sum(x in t for x in puntosBuenos) # puntos internos buenos
-                triangulos[t] = cantBuenos + 1 # +1 porque contamos uno de los vertices del triangulo
+                triangulos.setdefault(t[0], []).append((t, cantBuenos + 1))
+                # triangulos[t] = cantBuenos + 1 # +1 porque contamos uno de los vertices del triangulo
 
 # for k in triangulos:
 #     triangulos[k].sort(key = lambda k: point((0,1)).angle(k[0][1] - k[0][0]))
     # for t in triangulos[k]
+if not triangulos:
+    print(min(2, h))
+    exit()
+# print("triangulos: ", triangulos)
 maximo = 0
 for i in range(h):
     actual = puntosBuenos[i]
+    if not actual in triangulos: break
     vecinos = sorted(puntosBuenos[i+1:], key = lambda v : point((0,1)).angle(v - actual)) #ordeno los puntos de la derecha en sentido horario desde la vertical
-    for j in range(len(vecinos)): #chequear estos rangos
-        for k in range(j+1, len(vecinos)):
-            t = triangle(actual, vecinos[j], vecinos[k])
-            if t in triangulos:
-# print(triangulos)
+    sucesores = {}
+    valor = {}
+    for (a, b, c), cant in triangulos[actual]:
+        sucesores.setdefault(b, []).append(c)
+        valor[(b,c)] = cant
+    sucesoresConvexos = {}
+    # print(valor)
+    for (a, b, c), cant in triangulos[actual]:
+        sucesoresConvexos[(b,c)] = [x for x in sucesores[c] if (b - c).angle(x - c) > pi] if c in sucesores else []
+    # print(sucesoresConvexos)
+    for (b,c) in reversed(sorted(valor.keys())):
+        valor[(b,c)] += max([0] + [valor[(c,d)] for d in sucesoresConvexos[(b,c)]])
+    maximo = max([maximo] + list(valor.values()))
+    # for j in range(len(vecinos)): #chequear estos rangos
+    #     for k in range(j+1, len(vecinos)):
+    #         t = triangle(actual, vecinos[j], vecinos[k])
+print(maximo + 2)
