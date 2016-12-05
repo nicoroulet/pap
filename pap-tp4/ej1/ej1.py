5d349271bbf5f545a59af1f73b14fa367fe0a70b
Francisco Giordano <frangio.1@gmail.com>
2016-12-04 22:16:40 -0300

    usa clase point

--- pap-tp4/ej1/ej1.py
+++ pap-tp4/ej1/ej1.py
@@ -1,5 +1,17 @@
-from math import sqrt
+from math import sqrt, atan2, pi
 from Llist import *
+
+class point(tuple):
+    def __add__(self, other):
+        return point(s + o for s, o in zip(self, other))
+    def __sub__(self, other):
+        return point(s - o for s, o in zip(self, other))
+    def __abs__(self):
+        return sqrt(sum(s ** 2 for s in self))
+    def angle(self, other):
+        s = atan2(*reversed(self))
+        o = atan2(*reversed(other))
+        return (s - o) % (2 * pi)
 
 n = int(input())
 
@@ -7,39 +19,26 @@ p = None
 ady = dict()
 
 for _ in range(n-2):
-    x1, y1, x2, y2, x3, y3 = (int(w) for w in input().split())
+    x1, y1, x2, y2, x3, y3 = map(int, input().split())
+    v1, v2, v3 = map(point, [(x1, y1), (x2, y2), (x3, y3)])
 
-    v = min([(x1, y1), (x2, y2), (x3, y3)])
+    v = min([v1, v2, v3])
     if not p or v < p:
         p = v
 
-    ady.setdefault((x1, y1), set()).update([(x2, y2), (x3, y3)])
-    ady.setdefault((x2, y2), set()).update([(x1, y1), (x3, y3)])
-    ady.setdefault((x3, y3), set()).update([(x1, y1), (x2, y2)])
+    ady.setdefault(v1, set()).update([v2, v3])
+    ady.setdefault(v2, set()).update([v1, v3])
+    ady.setdefault(v3, set()).update([v1, v2])
 
-def sign(x):
-    return (x > 0) * 2 - 1
-
-def norm(v):
-    x, y = v
-    return sqrt(x ** 2 + y ** 2)
-
-def angulo(v, w):
-    vx, vy = v
-    wx, wy = w
-    cos = (vx * wx + vy * wy) / (norm(v) * norm(w))
-    sin = (vx * wy - vy * wx) / (norm(v) * norm(w))
-    return cos * sign(sin) + sign(sin)
-
-poly = Llist([(p[0], p[1] + 1), p, p])
+poly = Llist([p + (0, 1), p, p])
 
 it = poly.first
 for i in range(n):
     v = it.data
     w = it.next.data
-    a = sorted(ady[w], key=lambda u: (angulo((v[0] - w[0], v[1] - w[1]), (u[0] - w[0], u[1] - w[1])), u[0], u[1]))
+    a = sorted(ady[w], key=lambda u: ((v - w).angle(u - w), u[0], u[1]))
     auxIt = it
-    for j, u in enumerate(u for u in a if u != v):
+    for u in (u for u in a if u != v):
         if auxIt.next.next.data == u: break
         auxIt.next.insert(u)
         auxIt = auxIt.next
